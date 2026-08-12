5185735de7930396baf3d5a55083405be51761e6
Erick V <erick.hortensia.v10@gmail.com>
2026-08-11 19:49:52 -0700

    Test Music
    Testing Background Music

---
+++ Assets/Scripts/Sounds/Da Sound Test.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class DaSoundTest : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip _audioClip;
+    [SerializeField]
+    private AudioSource _audioSource;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _audioSource.clip = _audioClip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
